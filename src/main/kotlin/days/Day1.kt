9fdb27cec5351322b3f8872647730bbdcc78757a
Eric Broersma <git@ericbroersma.nl>
2022-12-05 23:05:26 +0100

    Day 1

--- src/main/kotlin/days/Day1.kt
+++ src/main/kotlin/days/Day1.kt
@@ -3,15 +3,28 @@ package days
 class Day1 : Day(1) {
 
     override fun partOne(): Any {
-        return inputList.take(2)
-            .map { it.uppercase() }
-            .joinToString(" ")
+        return inputList.fold(mutableListOf(mutableListOf<String>())) { acc, item-> 
+            if (item.isEmpty()) {
+                acc.add(mutableListOf())
+            } else {
+                acc.last().add(item)
+            }
+            acc
+        }.map {
+            it.map{ it.toInt() }.sum()
+        }.max()
     }
 
     override fun partTwo(): Any {
-        return inputString.split("\n")
-            .filterNot { it.isEmpty() }
-            .map { it.uppercase() }
-            .last()
+        return inputList.fold(mutableListOf(mutableListOf<String>())) { acc, item-> 
+            if (item.isEmpty()) {
+                acc.add(mutableListOf())
+            } else {
+                acc.last().add(item)
+            }
+            acc
+        }.map {
+            it.map{ it.toInt() }.sum()
+        }.sorted().reversed().take(3).sum()
     }
 }
